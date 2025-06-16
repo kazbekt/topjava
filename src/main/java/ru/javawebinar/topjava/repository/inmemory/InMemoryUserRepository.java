e60e2d6e7cec2412bec5402e2289a095989a9f4b
Kazbek T. <kazibo@gmail.com>
2025-06-17 00:37:46 +0300

    HW2.0 - InMemoryUserRepository

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -5,41 +5,65 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.util.UsersUtil;
 
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
 
-    @Override
-    public boolean delete(int id) {
-        log.info("delete {}", id);
-        return true;
+    private static final Comparator<User> USER_COMPARATOR = Comparator
+            .comparing(User::getName)
+            .thenComparing(User::getEmail);
+
+    private final Map<Integer, User> usersMap = new ConcurrentHashMap<>();
+    private final AtomicInteger counter = new AtomicInteger(0);
+
+    {
+       UsersUtil.users.forEach(this::save);
     }
 
     @Override
     public User save(User user) {
         log.info("save {}", user);
-        return user;
+        if(user.isNew()){
+            user.setId(counter.incrementAndGet());
+            usersMap.put(user.getId(), user);
+            return user;
+        }
+        return usersMap.computeIfPresent(user.getId(), (id, oldUser) -> user);
+    }
+
+    @Override
+    public boolean delete(int id) {
+        log.info("delete {}", id);
+        return usersMap.remove(id) != null;
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        return usersMap.get(id);
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        return usersMap.values().stream().sorted(USER_COMPARATOR).collect(Collectors.toList());
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
-        return null;
+        return usersMap.values().stream()
+                .filter(user -> email.equals(user.getEmail()))
+                .findFirst()
+                .orElse(null);
     }
 }
---
+++ src/main/java/ru/javawebinar/topjava/util/UsersUtil.java
@@ -0,0 +1,34 @@
+package ru.javawebinar.topjava.util;
+
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UsersUtil {
+
+    public static final User testUser = new User(
+            null, "Test Test", "test", "pass",
+            MealsUtil.DEFAULT_CALORIES_PER_DAY, true,
+            Arrays.asList(Role.USER, Role.ADMIN));
+
+    public static final List<User> users = Arrays.asList(
+            new User(null, "Ivan Ivanov", "email2", "pass", Role.USER),
+            new User(null, "Ivan Ivanov", "email1", "pass", Role.USER),
+            new User(null, "Petr Petrov", "email3", "pass", Role.USER),
+            new User(null, "Sergei Serge", "email4", "pass", Role.USER),
+            new User(null, "Pavel Pavlov", "email5", "pass", Role.USER)
+    );
+
+    public static void main(String[]args) {
+        InMemoryUserRepository repository = new InMemoryUserRepository();
+        repository.getAll().forEach(System.out::println);
+        repository.delete(4);
+        repository.getAll().forEach(System.out::println);
+        repository.save(testUser);
+        repository.getAll().forEach(System.out::println);
+        System.out.println(repository.getByEmail("test"));
+    }
+}
